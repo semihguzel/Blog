5cb84104ce226b6a4bddf2702a11fef041961c79
Semih Guzel <semih_guzel@outlook.com>
2020-06-10 20:54:53 +0300

    Context class have been added. Database connection has been set at OnConfiguring method. Property defined for accessing Blogs table and BlogMapping have been set with overriding OnModelCreating method.

---
+++ Blog.DAL/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blog.DATA.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.DAL.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .; Database = Blog; uid = sa; pwd = 1234");
+        }
+
+        public DbSet<Blog.DATA.Entities.Blog> Blogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Blog.DATA.Entities.Blog>(new BlogMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
