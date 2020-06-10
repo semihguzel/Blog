1b0b46bb3ba58e65561acaba4a1a711e0fb24488
Semih Guzel <semih_guzel@outlook.com>
2020-06-10 20:52:38 +0300

    EntityConfiguration has been added for Blog entity. Table name, primary key and column constraints have been defined on BlogMapping class.

---
+++ Blog.DATA/Mappings/BlogMapping.cs
@@ -0,0 +1,20 @@
+﻿using Blog.DATA.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.DATA.Mappings
+{
+    public class BlogMapping : IEntityTypeConfiguration<Blog.DATA.Entities.Blog>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Blog.DATA.Entities.Blog> builder)
+        {
+            builder.ToTable("Blog");
+
+            builder.HasKey(x => x.BlogId);
+            builder.Property(x => x.BlogHeader).HasMaxLength(100);
+            builder.Property(x => x.BlogText).HasMaxLength(500);
+        }
+    }
+}
