aa5aacaca56fa46a4ec6346f27f258965011d923
Semih Guzel <semih_guzel@outlook.com>
2020-06-10 19:08:58 +0300

    DATA project have been created with Blog entity.

---
+++ Blog.DATA/Entities/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.DATA.Entities
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string BlogHeader { get; set; }
+        public string BlogText { get; set; }
+    }
+}
