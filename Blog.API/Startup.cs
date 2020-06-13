f151187fcaaf6ce6df3377ddf93d341e204d855a
Semih Guzel <semih_guzel@outlook.com>
2020-06-13 18:23:50 +0300

    Startup has been configured. Dal and Manager objects will be created when their interfaces being called.

--- Blog.API/Startup.cs
+++ Blog.API/Startup.cs
@@ -2,9 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogApp.BLL.Abstract;
+using BlogApp.BLL.Concrete;
+using BlogApp.DAL.Abstract;
+using BlogApp.DAL.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +22,10 @@ namespace BlogApp.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IBlogDal, EfBlogDal>();
+            services.AddScoped<IBlogService, BlogManager>();
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +37,8 @@ namespace BlogApp.API
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStatusCodePages();
+            
         }
     }
 }
---
+++ Blog.DAL/Concrete/EfBlogDal.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.Core.DAL.EntityFramework;
+using BlogApp.DAL.Abstract;
+using BlogApp.DAL.Concrete;
+using BlogApp.DATA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.DAL.Concrete
+{
+    public class EfBlogDal : EfEntityRepositoryBase<Blog, Context>, IBlogDal
+    {
+    }
+}
